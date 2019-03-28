ead6d63ae734c310eb921d846b26c6eeeb935836
Julia Aseeva <julia_ajs@mail.ru>
2019-03-28 08:25:17 +0300

    reviews section on Vue. firstVersion

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,15 +1,43 @@
 import Vue from 'vue';
+import Flickity from 'vue-flickity';
 
 new Vue({
   el: "#reviews-component",
   template: "#reviews-list",
-  data(){
-    return{
-      reviews: {}
-    }
+  components: {
+    Flickity
+  },
+  data() {
+    return {
+      reviews: {},
+      strafe: 0,
+      flickityOptions: {
+        cellAlign: 'left',
+        groupCells: 2,
+        wrapAround: false,
+        prevNextButtons: false,
+        pageDots: false,
+        freeScroll: false,
+      }
+    };
+  },
+  methods: {
+    slide(direction) {
+      switch (direction) {
+        case 'prev':
+          this.$refs.flickity.previous();
+          break;
+        case 'next':
+          this.$refs.flickity.next();
+          break;
+      }
+    },
   },
   created(){
     const data = require("../data/reviews.json");
     this.reviews = data;
+  },
+  mounted() {
+
   }
 })
